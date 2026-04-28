085e5c0c074df198f6899b351bc93ec9c9fe099f
AlonsoTorr <66113023+AlonsoTorr@users.noreply.github.com>
2026-04-28 01:34:01 -0700

    Add files via upload

--- labatidora.js
+++ labatidora.js
@@ -3,13 +3,14 @@ function labatidora() {
 <div class="card">
       <button onclick="toggle(this)"> LA BATIDORA </button>
       <div class="content hidden">
-      Sentado en el borde de la piscina, con el culo lo más hacia el borde posible y las
-      manos por detrás de nosotros apoyadas en el suelo.   <br>
-      Realizar movimientos de pataleo en el agua con la punta del pie estirada y el tobillo relajado
-        <br><br>
-
+      Sentado en el borde de la piscina, lo más hacia el borde posible y las
+      manos por detrás de nosotros apoyadas en el suelo.
+  <br> Realizar movimientos de pataleo en el agua con la punta del pie estirada y el tobillo relajado
+        
+      <br><br>
         Min: <input type="number" value="1" min="1">
-        <br><br>
+      <br><br>
+
         <button class="start" onclick="startTimer(this)">▶️ Iniciar</button>
         <button class="pause" onclick="pauseTimer(this)">⏸ Pausar</button>
         <button class="reset" onclick="resetTimer(this)">🔄 Reiniciar</button>
